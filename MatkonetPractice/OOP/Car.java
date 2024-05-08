dfaa4b7e4e79802cb6369dee199cdf602ff23c47
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2024-05-09 02:11:43 +0300

    Update Car.java

--- MatkonetPractice/OOP/Car.java
+++ MatkonetPractice/OOP/Car.java
@@ -49,5 +49,19 @@ public class Car {
         return false;
     }
 
+    public static void main(String[] args)
+    {
+        Car car1 = new Car("69420", true, 500);
+        Car car2 = new Car("1212", false, 1276);
+        Car car3 = new Car("21314", true, 1312);
+        Car car4 = new Car("1511", false, 133);
+        AllCars temp = new AllCars(4);
 
+        temp.addCar(new Car("1002", true, 1000));
+
+        for(int i = 0; i<temp.getCars().length; i++)
+        {
+            System.out.println(temp.getCars()[i].getLicenseNum());
+        }
+    }
 }
